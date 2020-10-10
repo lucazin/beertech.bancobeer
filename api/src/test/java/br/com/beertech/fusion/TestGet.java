6fb1f46884a2e0e701c483658a0d620be73ec328
Patricia Crevelario Lisboa <Patricia.Crevelario.Lisboa1@ibm.com>
2020-10-10 15:37:26 -0300

    Adding tests cases for rest api

---
+++ api/src/test/java/br/com/beertech/fusion/TestGet.java
@@ -0,0 +1,14 @@
+package br.com.beertech.fusion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGet {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

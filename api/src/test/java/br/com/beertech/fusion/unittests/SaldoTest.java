eac20ffc21feb987925e366794506f8cdcf59a93
Maria Luisa Souza Matos <marialuisa@infox.com.br>
2020-10-10 15:55:13 -0300

    Ajuste nos testes de saldo

--- api/src/test/java/br/com/beertech/fusion/unittests/SaldoTest.java
+++ api/src/test/java/br/com/beertech/fusion/unittests/SaldoTest.java
@@ -1,6 +1,6 @@
 package br.com.beertech.fusion.unittests;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,24 +13,24 @@ import br.com.beertech.fusion.domain.Saldo;
 import br.com.beertech.fusion.service.SaldoService;
 import br.com.beertech.fusion.service.impl.SaldoServiceImpl;
 
-public class SaldoTeste {
+public class SaldoTest {
 
     @Test
     void testaSaldoDeposito() {
         List<OperacaoDto> operacoes = new ArrayList<>();
         operacoes.add(new OperacaoDto(OperationType.DEPOSITO, 100.));
         SaldoService saldoService = new SaldoServiceImpl();
-        assertThat(saldoService.calcularSaldo(operacoes)).isEqualTo(new Saldo(100.));
+        assertEquals(saldoService.calcularSaldo(operacoes), new Saldo(100.));
     }
-    
+
     @Test
     void testaSaldoSaque() {
         List<OperacaoDto> operacoes = new ArrayList<>();
         operacoes.add(new OperacaoDto(OperationType.SAQUE, 10.));
         SaldoService saldoService = new SaldoServiceImpl();
-        assertThat(saldoService.calcularSaldo(operacoes)).isEqualTo(new Saldo(-10.));
+        assertEquals(saldoService.calcularSaldo(operacoes), new Saldo(-10.));
     }
-    
+
     @Test
     void testaSaldoOperacoesVariadas() {
         List<OperacaoDto> operacoes = new ArrayList<>();
@@ -38,13 +38,13 @@ public class SaldoTeste {
         operacoes.add(new OperacaoDto(OperationType.SAQUE, 25.));
         operacoes.add(new OperacaoDto(OperationType.SAQUE, 10.));
         SaldoService saldoService = new SaldoServiceImpl();
-        assertThat(saldoService.calcularSaldo(operacoes)).isEqualTo(new Saldo(65.));
+        assertEquals(saldoService.calcularSaldo(operacoes), new Saldo(65.));
     }
-    
+
     @Test
     void testaSaldoSemOperacoes() {
         List<OperacaoDto> operacoes = new ArrayList<>();
         SaldoService saldoService = new SaldoServiceImpl();
-        assertThat(saldoService.calcularSaldo(operacoes)).isEqualTo(new Saldo(0.));
+        assertEquals(saldoService.calcularSaldo(operacoes), new Saldo(0.));
     }
 }

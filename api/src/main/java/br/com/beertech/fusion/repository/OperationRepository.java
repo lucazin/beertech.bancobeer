0f23356b66eeccbe09382e1da1a172f8d698c928
LuC4s Bérgamo <bergamo86@gmail.com>
2020-10-13 21:06:34 -0300

    Remove

--- api/src/main/java/br/com/beertech/fusion/repository/OperationRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.beertech.fusion.repository;
-
-import br.com.beertech.fusion.domain.Operacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperationRepository extends JpaRepository<Operacao, Long> {
-
-}

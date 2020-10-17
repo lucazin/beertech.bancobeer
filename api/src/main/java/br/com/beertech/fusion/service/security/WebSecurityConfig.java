060e55907455438277b5b46d0483581f7f586aee
LuC4s Bérgamo <bergamo86@gmail.com>
2020-10-17 13:46:34 -0300

    Update Swagger Config
    Update Swagger Config

--- api/src/main/java/br/com/beertech/fusion/service/security/WebSecurityConfig.java
+++ api/src/main/java/br/com/beertech/fusion/service/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -47,6 +48,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs",
+				"/configuration/ui",
+				"/swagger-resources/**",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"/webjars/**");
 	}
 
 	@Override
--- api/src/main/java/br/com/beertech/fusion/util/SwaggerConfiguration.java
+++ api/src/main/java/br/com/beertech/fusion/util/SwaggerConfiguration.java
@@ -1,36 +1,62 @@
 package br.com.beertech.fusion.util;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.servlet.ServletContext;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/bankbeer/.*";
+
     @Bean
-    public Docket api(ServletContext servletContext) {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(apiKey()))
-                .useDefaultResponseMessages(false).apiInfo(apiInfo())
-                .securityContexts(Collections.singletonList(securityContext()));
+    public Docket swaggerSpringfoxDocket() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+        apiInfoBuilder.title("BeerTech Developer Program");
+        apiInfoBuilder.description("Legacy POC / Grupo Fusion - Controle Bancário");
+        apiInfoBuilder.version("V1.0.0");
+        apiInfoBuilder.contact(new Contact("Grupo Fusion", ":)", "n/a"));
+        apiInfoBuilder.license("GNU General Public License");
+        apiInfoBuilder.licenseUrl("https://www.gnu.org/licenses/gpl-3.0.en.html");
+        apiInfoBuilder.termsOfServiceUrl("https://github.com/lucazin");
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .apiInfo(ApiInfo.DEFAULT)
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .ignoredParameterTypes(Pageable.class)
+                .ignoredParameterTypes(java.sql.Date.class)
+                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false).apiInfo(apiInfo());
+
+        docket = docket.select()
+                .paths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+
+        return docket;
     }
 
     private ApiInfo apiInfo() {
@@ -38,25 +64,30 @@ public class SwaggerConfiguration {
         apiInfoBuilder.title("BeerTech Developer Program");
         apiInfoBuilder.description("Legacy POC / Grupo Fusion - Controle Bancário");
         apiInfoBuilder.version("V1.0.0");
-        apiInfoBuilder.contact(new Contact("Grupo Fusion", ":)",
-                "n/a"));
+        apiInfoBuilder.contact(new Contact("Grupo Fusion", ":)", "n/a"));
         apiInfoBuilder.license("GNU General Public License");
         apiInfoBuilder.licenseUrl("https://www.gnu.org/licenses/gpl-3.0.en.html");
         apiInfoBuilder.termsOfServiceUrl("https://github.com/lucazin");
         return apiInfoBuilder.build();
     }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/.*")).build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
-        return Collections.singletonList(new SecurityReference("Bearer", authorizationScopes));
-    }
-
     private ApiKey apiKey() {
-        return new ApiKey("Token", "Authorization", "header");
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
     }
 }

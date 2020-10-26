5a9133e5d69b5ab8a9654bc4bd0900f2070c5776
Sabrina <sabrina_jodely@hotmail.com>
2020-10-26 12:54:26 -0300

    Inclusão do pre-authoraize no endpoint allAccount

--- api/src/main/java/br/com/beertech/fusion/controller/CurrentAccountController.java
+++ api/src/main/java/br/com/beertech/fusion/controller/CurrentAccountController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,7 @@ public class CurrentAccountController {
 
   @ApiIgnore
   @GetMapping("/allAccount")
+  @PreAuthorize("hasRole('ROLE_MODERATOR')")
   public ResponseEntity<List<CurrentAccount>> getAllAccounts() {
     return new ResponseEntity<>(currentAccountService.listAccounts(), HttpStatus.OK);
   }
